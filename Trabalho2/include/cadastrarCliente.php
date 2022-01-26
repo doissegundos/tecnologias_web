81a6b8143ce87b5f224c4a9606c9ff3a7cf11319
stefane <stefaneadnas@gmail.com>
2022-01-26 11:53:25 -0300

    adicionando q1

---
+++ Trabalho2/include/cadastrarCliente.php
@@ -0,0 +1,63 @@
+<?php
+if (isset($_SESSION['administrador'])){
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <form method="POST" action="">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email-cliente" aria-describedby="emailHelp" name="emailCliente" >
+                    <div id="emailHelp" class="form-text"></div>
+                </div>
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="nome-cliente" aria-describedby="nomeHelp" name="nomeCliente" >
+                    <div id="nomeHelp" class="form-text"></div>
+                </div>
+               
+                <div class="mb-3">
+                    <label for="cpf" class="form-label">CPF</label>
+                    <input type="text" class="form-control" id="cpf-cliente" aria-describedby="cpfHelp" name="cpfCliente" >
+                    <div id="cpf" class="form-text"></div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="celular" class="form-label">Celular</label>
+                    <input type="text" class="form-control" id="celular-cliente" aria-describedby="celularHelp" name="celularCliente" >
+                    <div id="celular" class="form-text"></div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="end-rua" class="form-label">Rua</label>
+                    <input type="text" class="form-control" id="end-rua-cliente" aria-describedby="endRuaHelp" name="endRuaCliente" >
+                    <div id="end-rua" class="form-text"></div>
+                </div>
+                <div class="mb-3">
+                    <label for="end-cidade" class="form-label">Cidade</label>
+                    <input type="text" class="form-control" id="end-cidade-cliente" aria-describedby="endCidadeHelp" name="endCidadeCliente" >
+                    <div id="end-cidade" class="form-text"></div>
+                </div>
+                <div class="mb-3">
+                    <label for="end-estado" class="form-label">Estado</label>
+                    <input type="text" class="form-control" id="end-estado-cliente" aria-describedby="endEstadoHelp" name="endEstadoCliente" >
+                    <div id="end-estado" class="form-text"></div>
+                </div>
+               
+                
+                
+                <input type="hidden" name="formCadastrarCliente">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+
+        </div>
+    </div>
+</div>
+<?php
+
+}else{
+    header("Location:../index.html");
+}
+?>
