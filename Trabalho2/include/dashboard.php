a405e8b060811c2431b0bcaf2617172aaff5d139
stefane <stefaneadnas@gmail.com>
2022-01-29 18:39:51 -0300

    fim trab

--- Trabalho2/include/dashboard.php
+++ Trabalho2/include/dashboard.php
@@ -515,6 +515,41 @@ include_once("../classes/Conexao.php");
                             </div>
                         </div>
                     </div>
+                    <?php
+                        $objConexao= new Conexao();
+                        $conexaoBD= $objConexao->getConexao();
+                        $sql="select * from consulta";
+                        $retornoBD=$conexaoBD-> query($sql);
+                        
+                        if ($retornoBD != null) {
+                        ?>
+                            <h1 >Consultas cadastradas</h1>
+                            <br/>
+                            <div class="row">
+                                <div class="col-12">
+                                    <table class="table table-striped table-hover">
+                                        <tr>
+                                            <th width="20%">#</th>
+                                            <th width="20%">Nome Pet</th>
+                                            <th width="20%">Data</th>
+                                            <th width="20%">Hora</th>
+                                            <th width="20%">Observações</th>
+                                        </tr>
+                        
+                                        <?php
+                        
+                                        while ($retorno = $retornoBD->fetch_object()) {
+                                            echo '<tr><td>' . $retorno->id_consulta . '</td><td>' .
+                                                $retorno->nome_pet . '</td><td>' .
+                                                $retorno->data_consulta . '</td><td>' .
+                                                $retorno->hora_consulta . '</td><td>'.
+                                                $retorno->obs_consulta . '</td>';
+                                        }
+                                    }
+                                        ?>
+                                    </table>
+                                </div>
+                            </div>
 
                         
 
